2692c61643ae6fd491b1b527fe5df4392486b736
Rok Mandeljc <rok.mandeljc@fe.uni-lj.si>
2015-11-27 13:14:45 +0100

    Updated affine_visualize_correspondences() to draw both original and projected points.

--- affine_visualize_correspondences.m
+++ affine_visualize_correspondences.m
@@ -1,14 +1,10 @@
-function fig = affine_visualize_correspondences(I1, I2, pts1, pts2, pts1p, pts2p, i1, i2)
-    assert(numel(i1) == numel(i2), 'Mismatch in number of selected points!');
+function fig = affine_visualize_correspondences(I1, I2, pts1, pts2, pts1p, pts2p, idx1, idx2)
+    assert(numel(idx1) == numel(idx2), 'Mismatch in number of selected points!');
     
-    num_points = numel(i1);
+    num_points = numel(idx1);
     
     % Allocate colors
     colors = rand(3, num_points);
-
-    % Select points
-    selected_pts1 = pts1(:, i1);
-    selected_pts2 = pts2(:, i2);
     
     %% New plot
     fig = figure('Name', 'Correspondences');
@@ -16,13 +12,15 @@ function fig = affine_visualize_correspondences(I1, I2, pts1, pts2, pts1p, pts2p
     % First image
     ha = tight_subplot(1, 2, 0, 0, 0);
     axes(ha(1));
-    imshow(I1);
-    scatter(selected_pts1(1,:), selected_pts1(2,:), [], colors');
+    imshow(I1); hold on;
+    scatter(pts1(1,idx1), pts1(2,idx1), [], colors', '+');
+    scatter(pts2p(1,idx2), pts2p(2,idx2), [], colors', 'x');
     
     % Second image
     axes(ha(2));
-    imshow(I2);
-    scatter(selected_pts2(1,:), selected_pts2(2,:), [], colors');
-    
+    imshow(I2); hold on;
+    scatter(pts2(1,idx2), pts2(2,idx2), [], colors', '+');
+    scatter(pts1p(1,idx1), pts1p(2,idx1), [], colors', 'x');
+
     drawnow();
 end
