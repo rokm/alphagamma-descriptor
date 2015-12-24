128b8cfa3f348323f0c5ca934e459af5241f7bf3
Rok Mandeljc <rok.mandeljc@fe.uni-lj.si>
2015-12-24 11:34:22 +0100

    Affine: added scale experiments

--- AffineDataset.m
+++ AffineDataset.m
@@ -48,9 +48,9 @@ classdef AffineDataset
             %  - H12: homography between both images
 
             % Validate parameters
+            assert(ismember(sequence, self.valid_sequences), 'Invalid sequence name!');
             assert(i1 == 1, 'First image must be image #1!');
             assert(i2 >= 2 && i2 <= 6, 'Second image must be image #2..#6!');
-            assert(ismember(sequence, self.valid_sequences), 'Invalid sequence name!');
 
             % Load
             data_path = fullfile(self.dataset_path, sequence);
@@ -64,11 +64,11 @@ classdef AffineDataset
             % [ I1, I2, H12 ] = GET_ROTATED_IMAGE (self, sequence, img, angle)
             %
             % Loads an image from the affine dataset and rotates it for the
-            % specified angle.
+            % specified angle around the center.
             %
             % Input:
             %  - sequence: sequence name
-            %  - i: image to use
+            %  - img: image to use
             %  - angle: angle to rotate (in degrees)
             %
             % Output:
@@ -81,8 +81,8 @@ classdef AffineDataset
             % Matlab's 1-based image coordinate system)
 
             % Validate parameters
-            assert(img >= 1 && img <= 6, 'Second image must be image #1..#6!');
             assert(ismember(sequence, self.valid_sequences), 'Invalid sequence name!');
+            assert(img >= 1 && img <= 6, 'Image must be #1..#6!');
 
             % Load
             data_path = fullfile(self.dataset_path, sequence);
@@ -107,6 +107,42 @@ classdef AffineDataset
                    0, 0, 1 ];
 
             H12 = T2*R*T1;
+        end
+        
+        function [ I1, I2, H12 ] = get_scaled_image (self, sequence, img, scale)
+            % [ I1, I2, H12 ] = GET_ROTATED_IMAGE (self, sequence, img, scale)
+            %
+            % Loads an image from the affine dataset and scales it with the
+            % specified scale factor.
+            %
+            % Input:
+            %  - sequence: sequence name
+            %  - img: image to use
+            %  - scale: scale factor
+            %
+            % Output:
+            %  - I1: original image
+            %  - I2: scaled image
+            %  - H12: homography between both images
+            %
+            % NOTE: following the OpenCV keypoint convention, the returned 
+            % homography assumes a 0-based coordinate system (unlike the
+            % Matlab's 1-based image coordinate system)
+
+            % Validate parameters
+            assert(ismember(sequence, self.valid_sequences), 'Invalid sequence name!');
+            assert(img >= 1 && img <= 6, 'Image must be #1..#6!');
+
+            % Load
+            data_path = fullfile(self.dataset_path, sequence);
+
+            I1 = imread( fullfile(data_path, sprintf('img%d.ppm', img)) );
+            I2 = imresize(I1, scale, 'bilinear');
+
+            % Construct the homography
+            H12 = [ scale,      0, 0; 
+                        0,  scale, 0;
+                        0,      0, 1 ];
         end
     end
 end
---
+++ affine_run_experiments_scale.m
@@ -0,0 +1,159 @@
+% AFFINE_RUN_EXPERIMENTS_SCALE
+%
+% Runs all scale experiments on Affine Dataset sequences.
+%
+% (C) 2015, Rok Mandeljc <rok.mandeljc@fri.uni-lj.si>
+
+clear;
+close all;
+
+%% Settings
+project_keypoints = true; % Project points instead of matching them
+visualize = false;
+result_dir = 'results-affine-scale';
+
+sequences = { 'wall', 'graffiti' };
+image = 1;
+scales = 1.00:0.05:1.50;
+
+% Distinction between results with matching and those with projection
+if project_keypoints,
+    result_dir = [ result_dir, '-proj' ];
+end
+
+%% Process
+if ~exist(result_dir, 'dir'),
+    mkdir(result_dir);
+end
+
+for s = 1:numel(sequences),
+    sequence = sequences{s};
+
+    %% Experiment: SURF keypoints, oriented descriptors
+    experiment_name = sprintf('%s-%d-surf-oriented', sequence, image);
+    experiment_title = sprintf('%s #%d - SURF keypoints (oriented)', sequence, image);
+
+    % Perform experiment if necessary
+    result_file = sprintf('%s-results.mat', experiment_name);
+    result_file = fullfile(result_dir, result_file);
+    if ~exist(result_file, 'file'),
+        keypoint_detector = vicos.keypoint_detector.SURF('HessianThreshold', 400, 'NOctaves', 3, 'NOctaveLayers', 4);
+
+        descriptors = {};
+        descriptors(end+1,:) = { 'SURF',  vicos.descriptor.SURF() };
+        descriptors(end+1,:) = { 'O-BRIEF64', vicos.descriptor.BRIEF('Bytes', 64, 'UseOrientation', true) };
+        descriptors(end+1,:) = { 'O-LATCH64', vicos.descriptor.LATCH('Bytes', 64, 'RotationInvariance', true) };
+
+        descriptors(end+1,:) = { 'O-\alpha\gamma simple', vicos.descriptor.AlphaGamma('orientation', true, 'extended', false, 'sampling', 'simple', 'use_scale', false, 'base_sigma', sqrt(2)) };
+        descriptors(end+1,:) = { 'O-\alpha\gamma C55x2', vicos.descriptor.AlphaGamma('orientation', true, 'extended', true, 'sampling', 'gaussian', 'use_scale', false) };
+        descriptors(end+1,:) = { 'O-\alpha\gamma C23x2', vicos.descriptor.AlphaGamma('orientation', true, 'extended', true, 'sampling', 'gaussian', 'use_scale', false, 'num_rays', 23) };
+
+        results = affine_batch_experiment_scale(keypoint_detector, descriptors, 'sequence', sequence, 'image', image, 'scales', scales, 'project_keypoints', project_keypoints);
+        results.title = experiment_title;
+
+        save(result_file, '-struct', 'results');
+    else
+        fprintf('Using cached results: %s\n', result_file);
+        results = load(result_file);
+    end
+
+    if visualize,
+        visualize_results_rotation(results);
+    end
+
+    %% Experiment: SURF keypoints, unoriented descriptors
+    experiment_name = sprintf('%s-%d-surf-unoriented', sequence, image);
+    experiment_title = sprintf('%s #%d - SURF keypoints (unoriented)', sequence, image);
+
+    % Perform experiment if necessary
+    result_file = sprintf('%s-results.mat', experiment_name);
+    result_file = fullfile(result_dir, result_file);
+    if ~exist(result_file, 'file'),
+        keypoint_detector = vicos.keypoint_detector.SURF('HessianThreshold', 400, 'NOctaves', 3, 'NOctaveLayers', 4, 'UpRight', true);
+
+        descriptors = {};
+        descriptors(end+1,:) = { 'U-SURF',  vicos.descriptor.SURF('UpRight', true) };
+        descriptors(end+1,:) = { 'U-BRIEF64', vicos.descriptor.BRIEF('Bytes', 64, 'UseOrientation', false) };
+        descriptors(end+1,:) = { 'U-LATCH64', vicos.descriptor.LATCH('Bytes', 64, 'RotationInvariance', false) };
+
+        descriptors(end+1,:) = { 'U-\alpha\gamma simple', vicos.descriptor.AlphaGamma('orientation', false, 'extended', false, 'sampling', 'simple', 'use_scale', false, 'base_sigma', sqrt(2)) };
+        descriptors(end+1,:) = { 'U-\alpha\gamma C55x2', vicos.descriptor.AlphaGamma('orientation', false, 'extended', true, 'sampling', 'gaussian', 'use_scale', false) };
+        descriptors(end+1,:) = { 'U-\alpha\gamma C23x2', vicos.descriptor.AlphaGamma('orientation', false, 'extended', true, 'sampling', 'gaussian', 'use_scale', false, 'num_rays', 23) };
+
+        results = affine_batch_experiment_scale(keypoint_detector, descriptors, 'sequence', sequence, 'image', image, 'scales', scales, 'project_keypoints', project_keypoints);
+        results.title = experiment_title;
+
+        save(result_file, '-struct', 'results');
+    else
+        fprintf('Using cached results: %s\n', result_file);
+        results = load(result_file);
+    end
+
+    if visualize,
+        visualize_results_rotation(results);
+    end
+
+    %% Experiment: SURF keypoints, oriented descriptors
+    experiment_name = sprintf('%s-%d-sift-oriented', sequence, image);
+    experiment_title = sprintf('%s #%d - SIFT keypoints (oriented)', sequence, image);
+
+    % Perform experiment if necessary
+    result_file = sprintf('%s-results.mat', experiment_name);
+    result_file = fullfile(result_dir, result_file);
+    if ~exist(result_file, 'file'),
+        keypoint_detector = vicos.keypoint_detector.SIFT();
+
+        descriptors = {};
+        descriptors(end+1,:) = { 'SIFT',  vicos.descriptor.SIFT() };
+        descriptors(end+1,:) = { 'O-BRIEF64', vicos.descriptor.BRIEF('Bytes', 64, 'UseOrientation', true) };
+        descriptors(end+1,:) = { 'O-LATCH64', vicos.descriptor.LATCH('Bytes', 64, 'RotationInvariance', true) };
+
+        descriptors(end+1,:) = { 'O-\alpha\gamma simple', vicos.descriptor.AlphaGamma('orientation', true, 'extended', false, 'sampling', 'simple', 'use_scale', false, 'base_sigma', sqrt(2)) };
+        descriptors(end+1,:) = { 'O-\alpha\gamma C55x2', vicos.descriptor.AlphaGamma('orientation', true, 'extended', true, 'sampling', 'gaussian', 'use_scale', false) };
+        descriptors(end+1,:) = { 'O-\alpha\gamma C23x2', vicos.descriptor.AlphaGamma('orientation', true, 'extended', true, 'sampling', 'gaussian', 'use_scale', false, 'num_rays', 23) };
+
+        results = affine_batch_experiment_scale(keypoint_detector, descriptors, 'sequence', sequence, 'image', image, 'scales', scales, 'project_keypoints', project_keypoints);
+        results.title = experiment_title;
+
+        save(result_file, '-struct', 'results');
+    else
+        fprintf('Using cached results: %s\n', result_file);
+        results = load(result_file);
+    end
+
+    if visualize,
+        visualize_results_rotation(results);
+    end
+
+    %% Experiment: SIFT keypoints, unoriented descriptors
+    experiment_name = sprintf('%s-%d-sift-unoriented', sequence, image);
+    experiment_title = sprintf('%s #%d - SIFT keypoints (unoriented)', sequence, image);
+
+    % Perform experiment if necessary
+    result_file = sprintf('%s-results.mat', experiment_name);
+    result_file = fullfile(result_dir, result_file);
+    if ~exist(result_file, 'file'),
+        keypoint_detector = vicos.keypoint_detector.SIFT('UpRight', true);
+
+        descriptors = {};
+        descriptors(end+1,:) = { 'U-SIFT',  vicos.descriptor.SIFT() };
+        descriptors(end+1,:) = { 'U-BRIEF64', vicos.descriptor.BRIEF('Bytes', 64, 'UseOrientation', false) };
+        descriptors(end+1,:) = { 'U-LATCH64', vicos.descriptor.LATCH('Bytes', 64, 'RotationInvariance', false) };
+
+        descriptors(end+1,:) = { 'U-\alpha\gamma simple', vicos.descriptor.AlphaGamma('orientation', false, 'extended', false, 'sampling', 'simple', 'use_scale', false, 'base_sigma', sqrt(2)) };
+        descriptors(end+1,:) = { 'U-\alpha\gamma C55x2', vicos.descriptor.AlphaGamma('orientation', false, 'extended', true, 'sampling', 'gaussian', 'use_scale', false) };
+        descriptors(end+1,:) = { 'U-\alpha\gamma C23x2', vicos.descriptor.AlphaGamma('orientation', false, 'extended', true, 'sampling', 'gaussian', 'use_scale', false, 'num_rays', 23) };
+
+        results = affine_batch_experiment_scale(keypoint_detector, descriptors, 'sequence', sequence, 'image', image, 'scales', scales, 'project_keypoints', project_keypoints);
+        results.title = experiment_title;
+
+        save(result_file, '-struct', 'results');
+    else
+        fprintf('Using cached results: %s\n', result_file);
+        results = load(result_file);
+    end
+
+    if visualize,
+        visualize_results_rotation(results);
+    end
+end
