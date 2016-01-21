841b9a6c47fbd1bedea49f3784b2ca12ac291f66
Rok Mandeljc <rok.mandeljc@fri.uni-lj.si>
2016-01-21 17:27:35 +0100

    Make: use OpenCV 3.1

--- make.m
+++ make.m
@@ -1,2 +1,2 @@
-mex mex/compute_keypoint_distances.cpp -I/opt/opencv-3.0.0-matlab/include -L/opt/opencv-3.0.0-matlab/lib -lopencv_core
-mex +vicos/+descriptor/@AlphaGamma/private/alpha_gamma_distances.cpp -outdir +vicos/+descriptor/@AlphaGamma/private/ -I/opt/opencv-3.0.0-matlab/include -L/opt/opencv-3.0.0-matlab/lib -lopencv_core 
+mex mex/compute_keypoint_distances.cpp -I/opt/opencv-3.1.0-matlab/include -L/opt/opencv-3.1.0-matlab/lib -lopencv_core
+mex +vicos/+descriptor/@AlphaGamma/private/alpha_gamma_distances.cpp -outdir +vicos/+descriptor/@AlphaGamma/private/ -I/opt/opencv-3.1.0-matlab/include -L/opt/opencv-3.1.0-matlab/lib -lopencv_core 
