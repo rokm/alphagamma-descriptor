fedd401b423239b49723466004daf3d4d37094c7
Rok Mandeljc <rok.mandeljc@fri.uni-lj.si>
2016-03-23 21:46:45 +0100

    Added Matlab startup script

---
+++ start_matlab.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Matlab
+MATLABDIR=/usr/local/MATLAB/R2015b
+
+# OpenCV
+OPENCV_LIB=/opt/opencv-3.1.0-mexopencv/lib
+
+# Set path
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${OPENCV_LIB}
+
+# Run MATLAB
+${MATLABDIR}/bin/matlab
