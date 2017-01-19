b8eb96c7ee82885d7d3abb51fc15d4b47bcbf171
Rok Mandeljc <rok.mandeljc@gmail.com>
2017-01-19 23:21:48 +0100

    DtuRobotEvaluation: added a missing function to reconstruct_point_3d()

--- +vicos/+experiment/@DtuRobotEvaluation/reconstruct_point_3d.m
+++ +vicos/+experiment/@DtuRobotEvaluation/reconstruct_point_3d.m
@@ -313,7 +313,7 @@ function [ err, F, f, J ] = checkfJ (fun, x, varargin)
     % nonlinear least squares solver.
     %
     % Version 10.11.03
-    % This code is part of the toolbox �immoptibox� which was written by Hans Bruun Nielsen.
+    % This code is part of the toolbox immoptibox which was written by Hans Bruun Nielsen.
     % Department of Mathematics and Mathematical Modelling, Technical University of Denmark.
         
     err = 0;
@@ -359,4 +359,30 @@ function [ err, F, f, J ] = checkfJ (fun, x, varargin)
     if isinf(F)
         err = -5;
     end
+end
+
+function [h, mu] = geth (A,g,mu)
+    % function  [h, mu] = geth(A,g,mu)
+    %
+    % Solve  (Ah + mu*I)h = -g  with possible adjustment of  mu
+    %
+    % Version 10.11.03
+    % This code is part of the toolbox immoptibox which was written by Hans Bruun Nielsen.
+    % Department of Mathematics and Mathematical Modelling, Technical University of Denmark.
+    %
+    % Factorize with check of pos. def.
+    n = size(A,1);
+    chp = 1;
+    while chp
+        [ R, chp ] = chol(A + mu*eye(n));
+        if chp == 0  % check for near singularity
+            chp = rcond(R) < 1e-15;
+        end
+        if chp
+            mu = 10*mu;
+        end
+    end
+    
+    % Solve (R'*R)h = -g
+    h = R \ (R' \ (-g));
 end
