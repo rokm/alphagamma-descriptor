4c2b6c90ee40ca90f6d4604509686ed5a4eb4e28
Rok Mandeljc <rok.mandeljc@gmail.com>
2019-01-06 20:44:40 +0100

    compile_code.m: force the use of R2017b API on R2018a or newer
    Use the legacy separate complex API.
    
    Also fixed some bash-shell style comments that sneaked in earlier...

--- compile_code.m
+++ compile_code.m
@@ -12,11 +12,11 @@ function compile_code ()
     opencv_include_dir = fullfile(opencv_bin_dir, 'include');
 
     if ispc()
-        # Windows
+        % Windows
         opencv_library_dir = fullfile(opencv_bin_dir, vicos.utils.opencv.get_arch_id(), vicos.utils.opencv.get_compiler_id(), 'lib');
         opencv_libs = '-lopencv_core341';
     else
-        # Linux
+        % Linux
         opencv_library_dir = fullfile(opencv_bin_dir, 'lib64');
         opencv_libs = '-lopencv_core';
     end
@@ -25,11 +25,21 @@ function compile_code ()
     src_dir = '+vicos/+descriptor/@AlphaGamma/private';
     src_dir = fullfile(root_directory, src_dir);
     
+    % Extra options
+    if ~verLessThan('matlab', '9.4')
+        % R2018a or newer; use R2017b API (separate complex API)
+        mex_options = '-R2017b';
+    else
+        % For some reason, -R2017b and -largeArrayDims seem to be mutually
+        % exclusive, so specify it only if we are not specifying -R2017b
+        mex_options = '-largeArrayDims';
+    end
+    
     % alpha_gamma_distances_fast.cpp
     src_file = fullfile(src_dir, 'alpha_gamma_distances_fast.cpp');
-    mex('-largeArrayDims', 'CXXFLAGS="$CXXFLAGS -Wall -mpopcnt"', sprintf('-I"%s"', opencv_include_dir), sprintf('-L"%s"', opencv_library_dir), opencv_libs, src_file, '-outdir', src_dir);
+    mex(mex_options, 'CXXFLAGS="$CXXFLAGS -Wall -mpopcnt"', sprintf('-I"%s"', opencv_include_dir), sprintf('-L"%s"', opencv_library_dir), opencv_libs, src_file, '-outdir', src_dir);
     
     % convert_bytestring_to_bitstring.cpp
     src_file = fullfile(src_dir, 'convert_bytestring_to_bitstring.cpp');
-    mex('-largeArrayDims', 'CXXFLAGS="$CXXFLAGS -Wall"', sprintf('-I"%s"', opencv_include_dir), sprintf('-L"%s"', opencv_library_dir), opencv_libs, src_file, '-outdir', src_dir);
+    mex(mex_options, 'CXXFLAGS="$CXXFLAGS -Wall"', sprintf('-I"%s"', opencv_include_dir), sprintf('-L"%s"', opencv_library_dir), opencv_libs, src_file, '-outdir', src_dir);
 end
